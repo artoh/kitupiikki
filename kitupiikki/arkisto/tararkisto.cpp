70a21929d3c4319d64ff2c348dc84b60ddf5cba0
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2018-02-06 21:25:35 +0200

    Tar standardimmaksi

--- kitupiikki/arkisto/tararkisto.cpp
+++ kitupiikki/arkisto/tararkisto.cpp
@@ -62,26 +62,30 @@ bool TarArkisto::lisaaTiedosto(const QString &polku)
     QByteArray tiedostonnimi = info.fileName().toLocal8Bit();
 
     otsake.replace(0, tiedostonnimi.length(), tiedostonnimi);
-    otsake.replace(100, QByteArray("0000644").size() , QByteArray("0000644"));
+    otsake.replace(100, QByteArray("000644 ").size() , QByteArray("000644 "));
 
-    QByteArray koko = QString("%1").arg( info.size(), 11, 8, QChar('0') ).toLocal8Bit();
+    QByteArray koko = QString("%1 ").arg( info.size(), 11, 8, QChar('0') ).toLocal8Bit();
     otsake.replace( 124, koko.size(), koko );
 
-    QByteArray mtime = QString("%1").arg( info.fileTime(QFileDevice::FileModificationTime).toSecsSinceEpoch() , 11, 8, QChar('0') ).toLocal8Bit();
+    QByteArray mtime = QString("%1 ").arg( info.fileTime(QFileDevice::FileModificationTime).toSecsSinceEpoch() , 11, 8, QChar('0') ).toLocal8Bit();
 
     otsake.replace( 136, mtime.size(), mtime);
     otsake.replace( 156, QByteArray("0").size(), QByteArray("0"));
 
-    otsake.replace( 257, QByteArray("ustar  ").size(), QByteArray("ustar  "));
+    otsake.replace( 257, QByteArray("ustar").size(), QByteArray("ustar"));
+    otsake.replace( 263, QByteArray("00").size(), QByteArray("00"));
 
     otsake.replace( 297, QByteArray("users").size(), QByteArray("users"));
+
+    otsake.replace( 0x149, QByteArray("000000 ").size(), QByteArray("000000 "));
+    otsake.replace( 0x151, QByteArray("000000 ").size(), QByteArray("000000 "));
 
     quint32 tarkastussumma = 32 * 8;
     // Tarkastussumman laskenta
     for( int i=0; i < otsake.size(); i++)
         tarkastussumma += otsake.at(i);
 
-    QByteArray tarkaste = QString("%1").arg( tarkastussumma, 6, 8, QChar('0') ).toLocal8Bit();
+    QByteArray tarkaste = QString("%1 ").arg( tarkastussumma, 6, 8, QChar('0') ).toLocal8Bit();
     otsake.replace(148, tarkaste.length(), tarkaste);
 
     // Nyt otsake on valmis kirjoitettavaksi
