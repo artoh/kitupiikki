cb0a53958263b3f145d045b5581b397358153fe3
Petri Aarnio <petri@atfos.fi>
2018-09-23 15:52:37 +0300

    Poistettu VERSION_STRING:in generointi. Versio luetaan versio.h-tiedostosta.

--- mac/read_version.sh
+++ mac/read_version.sh
@@ -6,7 +6,5 @@
 #  Created by Petri Aarnio on 15/09/2018.
 #  
 
-VERSION=`/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' Info.plist`
-BUILD=`/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' Info.plist`
-
-echo "#define VERSION_STRING \"$VERSION ($BUILD)\"" > version.h
+KITUPIIKKI_VERSIO=`/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' Info.plist`
+KITUPIIKKI_BUILD=`/usr/libexec/PlistBuddy -c 'Print :CFBundleVersion' Info.plist`
