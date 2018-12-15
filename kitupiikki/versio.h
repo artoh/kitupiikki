4b2df7aa3a41b69772f80b009fafcf9a9bf18fa9
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2018-12-15 20:10:29 +0200

    1.3-beta.1

--- kitupiikki/versio.h
+++ kitupiikki/versio.h
@@ -24,7 +24,7 @@
   Kitupiikin versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITUPIIKKI_VERSIO "1.3-beta"
+#define KITUPIIKKI_VERSIO "1.3-beta.1"
 #define KITUPIIKKI_BUILD ""
 
 #endif // VERSIO_H
