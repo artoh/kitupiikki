4b30110b2b9818d079aeac824dd4282084264340
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2019-05-05 16:42:21 +0300

    #361 Myös tyhjät raportit

--- kitupiikki/raportti/paivakirjaraportti.cpp
+++ kitupiikki/raportti/paivakirjaraportti.cpp
@@ -285,6 +285,9 @@ RaportinKirjoittaja PaivakirjaRaportti::kirjoitaRaportti(QDate mista, QDate mihi
         kirjoittaja.lisaaRivi( summarivi );
     }
 
+    if( kirjoittaja.tyhja())
+        kirjoittaja.lisaaRivi();
+
     return kirjoittaja;
 }
 
