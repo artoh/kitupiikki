ac40a7948feeef051308684923fbe6da2eadf723
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2019-11-08 15:47:35 +0200

    Build

--- kitupiikki/versio.h
+++ kitupiikki/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "2.0-pre3"
-#define KITSAS_BUILD  ";)" // "PORTABLE" //
+#define KITSAS_BUILD  "o;)" // "PORTABLE" //
 
 #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
