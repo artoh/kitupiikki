81b8002bc77b0992001e3677a0c70923474dfbd5
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2020-01-16 22:39:40 +0200

    2.0-pre5-review1

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "2.0-pre5"
-#define KITSAS_BUILD  "X2" // "PORTABLE" //
+#define KITSAS_BUILD  "Review 1"
 
 #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 #define KITSAS_DEVEL
