b56e9ab53a959c8b9547fc5513040c341faaaefe
Petri Aarnio <petri@aarnio.biz>
2020-07-27 08:35:40 +0300

    Revert "Qt siirretty käyttäjän hakemistosta brew:n asennukseen"
    This reverts commit 55f1bc47565bd353f10fdbdf0fea2e2448accb42.

--- mac/update_project.sh
+++ mac/update_project.sh
@@ -6,4 +6,4 @@
 #  Created by Petri Aarnio on 14/02/2019.
 #  
 
-/usr/local/opt/qt/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
+~/Qt/5.14.1/clang_64/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
