8060a9ac9c529b92ed2a4cf1dae937e61cc76aa3
Petri Aarnio <petri@atfos.fi>
2020-08-07 08:30:01 +0300

    Kasattu käännösympäristö toimimaan macOS High Sierralla varustetussa koneessa. Homebrewlla asennetut poppler- ja zip-kirjastot vaativat Qt:n päivityksen 5.15.0-versioon.

--- mac/update_project.sh
+++ mac/update_project.sh
@@ -6,4 +6,4 @@
 #  Created by Petri Aarnio on 14/02/2019.
 #  
 
-~/Qt/5.14.1/clang_64/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
+~/Qt/5.15.0/clang_64/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
