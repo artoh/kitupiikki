c2122ac3ed925deb223c05e38cc2ad8e13d7918b
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2020-09-07 13:44:44 +0300

    Kooste C

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "2.0"
-#define KITSAS_BUILD  "B"
+#define KITSAS_BUILD  "C"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
