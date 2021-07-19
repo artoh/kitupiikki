54480395cc99d3fb7703a10f7384c516d1c58851
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2021-07-19 13:48:04 +0300

    B5

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "3.0-b+5"
-#define KITSAS_BUILD  "B4"
+#define KITSAS_BUILD  "B5"
 
 #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
