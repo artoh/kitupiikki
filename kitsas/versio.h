71c762a240f60aaf639cc1dcc9943861e4e4e0ff
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2021-08-07 06:24:49 +0300

    Beta+8

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,8 +24,8 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "3.0-b+7"
-#define KITSAS_BUILD  "B5"
+#define KITSAS_VERSIO "3.0-b+8"
+#define KITSAS_BUILD  "B8"
 
 #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
