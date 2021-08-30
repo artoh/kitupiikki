358dca6dea105dbdd44b4bb5b981f1e358daf784
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2021-08-30 17:42:36 +0300

    3.0 Build a

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "3.0"
-#define KITSAS_BUILD  "A"
+#define KITSAS_BUILD  "a"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
