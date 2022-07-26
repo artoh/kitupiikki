b8bf2ca0efc090b4e40cddbc4ea63ad10c857fcb
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2022-07-26 11:13:27 +0300

    SmtpClientin staattinen linkitys

--- kitsas/smtpclient/smtpmime_global.h
+++ kitsas/smtpclient/smtpmime_global.h
@@ -1,10 +1,19 @@
 #ifndef SMTPMIME_GLOBAL_H
 #define SMTPMIME_GLOBAL_H
 
+
+/*
+
+  Kitsaassa linkitetään staattisesti
+
 #ifdef SMTP_MIME_LIBRARY
 #define SMTP_MIME_EXPORT Q_DECL_EXPORT
 #else
 #define SMTP_MIME_EXPORT Q_DECL_IMPORT
 #endif
+
+*/
+
+#define SMTP_MIME_EXPORT
 
 #endif // SMTPMIME_GLOBAL_H
