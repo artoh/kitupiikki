c7794a90a4fab4e47ef95637d6790dd2bf577030
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2022-11-04 08:02:19 +0200

    beta.1

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,7 +24,7 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "3.3-beta"
+#define KITSAS_VERSIO "3.3-beta.1"
 #define KITSAS_BUILD  "B2"
 
 #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
