e48ebe17e4d7c3edd24bcab2a925427e396d7792
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2022-12-13 18:08:33 +0200

    4.0 alpha

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,7 +24,7 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "4.0-beta"
+#define KITSAS_VERSIO "4.0-alpha"
 #define KITSAS_BUILD  "D0"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
