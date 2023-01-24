724b33fa4ad68b381a62a5533b638dff886a386d
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2023-01-24 13:44:29 +0200

    4.0.1

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -27,7 +27,7 @@
 #define KITSAS_VERSIO "4.0.1"
 #define KITSAS_BUILD  "2401"
 
-#define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
+// #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
 
 #define KITSAS_API "https://api.kitsas.fi"
