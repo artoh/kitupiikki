ecaff1f712ec1ef213723d0944e08e329b468ddb
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2023-04-21 16:24:10 +0300

    v4.0.5

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,8 +24,8 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "4.0.4"
-#define KITSAS_BUILD  "1204"
+#define KITSAS_VERSIO "4.0.5"
+#define KITSAS_BUILD  "2104"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
