a564c00e5ad73d8ffdd8c03773f5e5e1b396bf47
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2023-07-15 11:26:29 +0300

    Beta.C

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -25,7 +25,7 @@
 */
 
 #define KITSAS_VERSIO "5.1-beta"
-#define KITSAS_BUILD  "B"
+#define KITSAS_BUILD  "C"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
