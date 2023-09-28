619a1207cee35399f3c542b5ca015078c907dc90
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2023-09-28 08:18:24 +0300

    5,2-alpha

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,7 +24,7 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "5.1.1"
+#define KITSAS_VERSIO "5.2-alpha"
 #define KITSAS_BUILD  "A"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
