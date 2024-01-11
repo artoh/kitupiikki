1613114f370ffbc0a4da6346c2c1a00ef3acb071
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2024-01-11 19:59:07 +0200

    5.3RC

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,7 +24,7 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "5.3-beta"
+#define KITSAS_VERSIO "5.3-RC"
 #define KITSAS_BUILD  "A"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
