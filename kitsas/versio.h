95555ad5b68d743c70d10a6ba31342117c70a3be
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2024-04-12 12:25:29 +0300

    5.5. Voikukka

--- kitsas/versio.h
+++ kitsas/versio.h
@@ -24,8 +24,8 @@
   Kitsaan versio määritellään tässä tiedostossa. Tiedosto voidaan myös generoida käännösaikaisesti.
 */
 
-#define KITSAS_VERSIO "5.5-beta.2"
-#define KITSAS_BUILD  "X"
+#define KITSAS_VERSIO "5.5"
+#define KITSAS_BUILD  "A"
 
 // #define KITSAS_PORTABLE  // Windowsin Portable-versiossa (ei asenneta)
 // #define KITSAS_DEVEL
