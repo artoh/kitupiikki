6ff2994244045140b8a5f67d1e07fc581b62c784
Petri Aarnio <petri@aarnio.biz>
2026-07-11 12:13:21 +0300

    Qt 6.11.1

--- mac/update_project.sh
+++ mac/update_project.sh
@@ -4,6 +4,13 @@
 #  kitsas
 #
 #  Created by Petri Aarnio on 14/02/2019.
-#  
+#
 
-~/Qt/6.7.3/macos/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
+cd "$(dirname "$0")"
+
+# qmake caches the macOS SDK version in .qmake.stash; remove stale entries
+# after Xcode upgrades (otherwise you get "building against version 12.1").
+rm -f .qmake.stash
+
+~/Qt/6.11.1/macos/bin/qmake -spec macx-xcode ../kitsas/kitsas.pro
+./qt_preprocess.sh
